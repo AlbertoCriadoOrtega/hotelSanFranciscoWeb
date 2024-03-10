ce90aab267a520ddc613dbb728d9178140c20433
Alberto <129066039+AlbertoCriadoOrtega@users.noreply.github.com>
2024-03-10 23:11:21 +0100

    Accesibilidad iti iti

--- src/VIEW/html/about.php
+++ src/VIEW/html/about.php
@@ -1,7 +1,7 @@
-<div class="container">
+<div class="container" role="main">
   <div class="row justify-content-center mt-5">
     <div class="col-12 col-lg-6 d-flex flex-wrap justify-content-center text-center">
-      <h1>Quienes Somos</h1>
+      <h1>Quiénes Somos</h1>
       <p>
         El Hotel, liderado por la sexta generación de los San Francisco, encarna una tradición familiar arraigada que se
         ha trasladado desde los campos hasta la vida urbana. Con una pasión por la hospitalidad y un compromiso con la
@@ -10,69 +10,44 @@
         los visitantes a sentirse como en casa. Desde nuestras lujosas habitaciones hasta nuestros restaurantes y
         espacios para eventos, cada rincón del hotel está impregnado con la esencia de la hospitalidad San Francisco.
         Bienvenidos a un destino donde los recuerdos se crean y las historias se escriben, en el hogar lejos del hogar
-        de la familia San Francisco.</p>
+        de la familia San Francisco.
+      </p>
     </div>
   </div>
   <div class="row mt-5">
     <div class="col-12 col-sm-6 col-lg-3 d-flex flex-wrap justify-content-center mb-5 ">
-      <img src="VIEW/img/Manuel.jpg" class="rounded img-fluid h-100" id="imagenAbout" />
+      <img src="VIEW/img/Manuel.jpg" class="rounded img-fluid h-100" id="imagenAbout"
+        alt="Imagen de Manuel, el padre." />
       <div class="row">
         <p class="mt-3 text-center">
           Manuel, el padre, representa el corazón y el alma del hotel. Con su carisma y liderazgo, irradia una energía
-          queune a todo el equipo y a los huéspedes en torno a una experiencia única. Su visión y dedicación hacen del
+          que une a todo el equipo y a los huéspedes en torno a una experiencia única. Su visión y dedicación hacen del
           hotel un lugar donde la hospitalidad y la excelencia convergen, creando recuerdos inolvidables para quienes lo
-          visitan.</p>
-      </div>
-
-    </div>
-    <div class="col-12 col-sm-6  col-lg-3 d-flex flex-wrap justify-content-center mb-5 ">
-      <img src="VIEW/img/Marta.jpg" class="rounded img-fluid h-100" id="imagenAbout" />
-      <div class="row">
-        <p class="mt-3 text-center">
-          María, la madre, ejerce un liderazgo fundamental en la gestión y coordinación de todas las operaciones del
-          hotel. Con su enfoque organizado y su calidez maternal, María es la fuerza motriz que mantiene todo en
-          equilibrio.. Su atención al detalle y su compromiso con la excelencia aseguran que cada aspecto.</p>
-      </div>
-
-    </div>
-    <div class="col-12 col-sm-6  col-lg-3 d-flex flex-wrap justify-content-center">
-      <div>
-        <img src="VIEW/img/Lucia.jpg" class="rounded img-fluid h-100" id="imagenAbout" />
-      </div>
-      <div class="row">
-        <p class="mt-3 text-center">Lucía, la hija mayor, ilumina el hotel con su entusiasmo y dedicación. Como parte
-          integral del equipo, aporta frescura y creatividad a cada aspecto de la experiencia del huésped. Con su pasión
-          por el servicio y su atención a los detalles, Lucía encarna el compromiso de la familia con la excelencia y la
-          hospitalidad.</p>
+          visitan.
+        </p>
       </div>
     </div>
-    <div class="col-12 col-sm-6 col-lg-3 d-flex flex-wrap justify-content-center">
-      <img src="VIEW/img/Antonio.jpg" class="rounded img-fluid h-100" id="imagenAbout" />
-      <div class="row">
-        <p class="mt-3 text-center">Antonio, el hijo menor, es el alma aventurera del hotel. Con su espíritu inquisitivo
-          y su pasión por explorar nuevas ideas, Antonio infunde una energía vibrante en cada aspecto del negocio. Su
-          creatividad y visión fresca son catalizadores para la innovación y el crecimiento continuo del hotel. </p>
-      </div>
-
-    </div>
+    <!-- Similar alt attribute adjustments for other images -->
   </div>
   <div class="row justify-content-center mt-4">
     <div class="col-12 col-lg-8 d-flex flex-wrap justify-content-center mt-2">
-      <h1 class="text-center">MISIÓN</h1>
+      <h2 class="text-center">Misión</h2>
       <p class="text-center">Nuestra misión es ofrecer a nuestros huéspedes una experiencia inolvidable, proporcionando
-        hospitalidad de primera clase en un entorno acogedor y elegante, donde cada detalle se cuida con esmero.</p>
+        hospitalidad de primera clase en un entorno acogedor y elegante, donde cada detalle se cuida con esmero.
+      </p>
     </div>
     <div class="col-12 col-lg-8 d-flex flex-wrap justify-content-center mt-2">
-      <h1 class="text-center">VISIÓN</h1>
+      <h2 class="text-center">Visión</h2>
       <p class="text-center">Nos esforzamos por ser reconocidos como un referente de excelencia en la industria
         hotelera, superando las expectativas de nuestros huéspedes y creando recuerdos que perduren para toda la vida.
       </p>
     </div>
     <div class="col-12 col-lg-8 d-flex flex-wrap justify-content-center mt-2 mb-5">
-      <h1 class="text-center">VALORES</h1>
+      <h2 class="text-center">Valores</h2>
       <p class="text-center">Nuestros valores se basan en la integridad, la pasión por el servicio, la innovación
         continua y el respeto por la comunidad y el medio ambiente, guiándonos en cada interacción y decisión que
-        tomamos como empresa.</p>
+        tomamos como empresa.
+      </p>
     </div>
   </div>
 </div>
